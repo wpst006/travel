a8da99d3c96eea997efbdcd89475e829e72319f7
Wai Phyo <waiphyo@globalsignin.com>
2014-03-04 17:53:51 +0630

    WPST

--- db/traveltour.sql
+++ db/traveltour.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 26, 2014 at 12:44 AM
+-- Generation Time: Mar 04, 2014 at 07:22 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `traveltour`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `booking_id` varchar(15) NOT NULL,
+  `booking_date` datetime NOT NULL,
+  `package_id` varchar(15) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `duration` varchar(50) NOT NULL,
+  `no_of_people` int(2) NOT NULL,
+  `airline_no` varchar(50) NOT NULL,
+  `route` varchar(50) NOT NULL,
+  PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,6 +62,69 @@ CREATE TABLE IF NOT EXISTS `customers` (
 
 INSERT INTO `customers` (`customer_id`, `firstname`, `lastname`, `passport_no`, `country`, `postalcode`, `phone_no`) VALUES
 ('CUS000002', 'a', 'a', 'a', 'a', 'a', 'a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotels`
+--
+
+CREATE TABLE IF NOT EXISTS `hotels` (
+  `hotel_id` varchar(15) NOT NULL,
+  `hotel_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`hotel_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `hotels`
+--
+
+INSERT INTO `hotels` (`hotel_id`, `hotel_name`) VALUES
+('HOT_000001', 'Sedona'),
+('HOT_000002', 'Park Royal'),
+('HOT_000003', 'Inya Lake');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packagetours`
+--
+
+CREATE TABLE IF NOT EXISTS `packagetours` (
+  `package_id` varchar(15) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `duration` varchar(50) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`package_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `packagetours`
+--
+
+INSERT INTO `packagetours` (`package_id`, `title`, `duration`, `price`) VALUES
+('PKG_000001', 'abcde', '5', '5.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packagetour_hotel`
+--
+
+CREATE TABLE IF NOT EXISTS `packagetour_hotel` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `hotel_id` varchar(15) NOT NULL,
+  `packagetour_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `packagetour_hotel`
+--
+
+INSERT INTO `packagetour_hotel` (`id`, `hotel_id`, `packagetour_id`) VALUES
+(14, 'HOT_000003', 'PKG_000001'),
+(15, 'HOT_000002', 'PKG_000001');
 
 -- --------------------------------------------------------
 
